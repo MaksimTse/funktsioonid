94de7c55382f2fe6d72ab78f6f6f001aad40b3a2
opilane <opilane@HP-CZC6297B8L>
2022-12-14 11:34:34 +0200

    update+new function

--- funktsioonid.py
+++ funktsioonid.py
@@ -5,35 +5,58 @@ from tkinter import Pack
 #Puu läbimõõdu arvutamine
 #Kirjuta programm, mis küsib kasutaja käest puu ümbermõõdu ning teatab selle peale puu läbimõõdu.
 #print("Puu läbimõõdu arvutamine")
-#C=float(input("Anna ümbermõõt: "))
-#d=round(C/pi,2)
-#print("Puu läbimõõt",d)
+#try:
+#    C=float(input("Anna ümbermõõt: "))
+#    if C>0:
+#        d=round(C/pi,2)
+#        print("Puu läbimõõt =",d)
+#    else:
+#        print("C peab olema suurem kui 0")
+#except:
+#    print("Andmetüüp on vale!")
 
 #2.
 #Arvutage Pythoni käsureal, kui pikk on ristkülikukujulise maatüki diagonaal, mille mõõtmed on Nm x Mm. N ja M küsi kasutajalt.
 #print("Ristkülikukujulise maatüki diagonaal")
-#N=float(input("Anna pikkus1: "))
-#M=float(input("Anna pikkus2: "))
-#A=round(sqrt(N**2+M**2),2)
-#print("Ristkülikukujulise maatüki diagonaal =",A,"m")
+#try:
+#    N=float(input("Anna pikkus1: "))
+#    M=float(input("Anna pikkus2: "))
+#    if N>0 and M>0:
+#        A=round(sqrt(N**2+M**2),2)
+#        print("Ristkülikukujulise maatüki diagonaal =",A)
+#    else:
+#        print("M ja N peab olema suurem kui 0")
+#except:
+#    print("Andmetüüp on vale!")
 
 #3.
-#Leidke järgnevast näiteprogrammist semantiline viga:
-#aeg = float(input("Mitu tundi kulus sõiduks? "))
-#teepikkus = float(input("Mitu kilomeetrit sõitsid? "))
-#kiirus = teepikkus / aeg
-#print("Sinu kiirus oli " + str(kiirus) + " km/h")
-
+# järgnevast näiteprogrammist semantiline viga:
+#try:
+#    aeg = float(input("Mitu tundi kulus sõiduks? "))
+#    teepikkus = float(input("Mitu kilomeetrit sõitsid? "))
+#    if aeg>0 and teepikkus>0:
+#        kiirus = teepikkus / aeg
+#        print("Sinu kiirus oli " + str(kiirus) + " km/h")
+#    else:
+#        print("aeg ja teepikkus peab olema suurem kui 0")
+#except:
+#    print("Andmetüüp on vale!")
 #4.
 #Koostada programm, mis arvutab aritmeetilise keskmise suvalisest etteantud 5 täis arvust.
 #print("aritmeetiline keskmine")
-#A1=int(input("Esimene arv: "))
-#A2=int(input("Teine arv: "))
-#A3=int(input("Kolmas arv: "))
-#A4=int(input("Neljas arv: "))
-#A5=int(input("Viies arv: "))
-#Ak=(A1+A2+A3+A4+A5)/5
-#print("aritmeetiline keskmine =",Ak)
+#try:
+#    A1=int(input("Esimene arv: "))
+#    A2=int(input("Teine arv: "))
+#    A3=int(input("Kolmas arv: "))
+#    A4=int(input("Neljas arv: "))
+#    A5=int(input("Viies arv: "))
+#    if A1>0 and A2>0 and A3>0 and A4>0 and A5>0:
+#        Ak=(A1+A2+A3+A4+A5)/5
+#        print("aritmeetiline keskmine =",Ak)
+#    else:
+#        print("andmed peab olema suurem kui 0")
+#except:
+#    print("Andmetüüp on vale!")
 
 #5.
 #Joonista samasugune konn
@@ -45,13 +68,15 @@ from tkinter import Pack
 #6.
 #Arvutame kolmnurga ümbermõõdu. Loo kolm täisarvulist muutujat a, b, c. Loo valem, mis arvutab kolmnurga ümbermõõdu (P=a+b+c)
 #print("Arvutame kolmnurga ümbermõõdu")
-#a=randint(0,100)
-#b=randint(0,100)
-#c=randint(0,100)
-#print(f"a={a} \nb={b}\nc={c}")
-#P=round(a+b+c,2)
-#print("Ümbermõõt =",P)
-
+#try:
+#    a=randint(0,150)
+#    b=randint(0,150)
+#    c=randint(0,150)
+#    print(f"a={a} \nb={b}\nc={c}")
+#    P=round(a+b+c,2)
+#    print("Ümbermõõt =",P)
+#except:
+#    print("Andmetüüp on vale!")
 #7.
 #Pitsa
 
@@ -74,24 +99,35 @@ from tkinter import Pack
     #Programm leiab kütusekulu 100km kohta keskmiselt
 
 #print("Kütusekulu arvutamine")
-#l=float(input("Kütuse liitrid kogus: "))
-#km=float(input("Läbitud kilomeetrid: "))
-#kulu=round((l/km)*100,2)
-#print(f"kütsekulu {kulu}")
+#try:
+#    l=float(input("Kütuse liitrid kogus: "))
+#    km=float(input("Läbitud kilomeetrid: "))
+#    if l>0 and km>0:
+#        kulu=round((l/km)*100,2)
+#        print(f"kütsekulu {kulu}")
+#    else:
+#        print("andmed peab olema suurem kui 0")
+#except:
+#    print("Andmetüüp on vale!")
 
-#9
+#9.
 #Rulluisutajad
 
     #Rulluisutaja keskmine kiirus on 29,9km/h
     #Kui kaugele jõuab M minutiga
 
 #print("Rulluisutajad")
-#M=int(input("Minutid: "))
-#M=M/60
-#tee=round(M*29.9,2)
-#print(f"Jõuab {tee} km")
-
-#10
+#try:
+#    M=int(input("Minutid: "))
+#    M=M/60
+#    if M>0:
+#        tee=round(M*29.9,2)
+#        print(f"Jõuab {tee} km")
+#    else:
+#        print("andmed peab olema suurem kui 0")
+#except:
+#    print("Andmetüüp on vale!")
+#10.
 #Ajateisendus
 
     #Kasutaja sisestab aja minutites
@@ -99,7 +135,32 @@ from tkinter import Pack
     #Näiteks: sisestus 72, vastus 1:12
 
 #print("Ajateisendus")
-#M=int(input("Sisets aja minutites: "))
-#H=M//60 #hours
-#M=M%60 #minutes
-#print(f"vastus {H}:{M}")
+#try:
+#    M=int(input("Sisets aja minutites: "))
+#    if M>0:
+#        H=M//60 #hours
+#        M=M%60 #minutes
+#        print(f"vastus {H}:{M}")
+#    else:
+#        print("andmed peab olema suurem kui 0")
+#except:
+#    print("Andmetüüp on vale!")
+
+#11.
+#print (a.isalpha(), a.isdigit()) - proverka vvedeni cifri ili slova
+#print("Ema roobot")
+#while True:
+#    try:
+#        a=int(input("Mis hinde sa koolis said?"))
+#        if a==5:
+#            print("Tubli poiss!")
+#        elif a==4:
+#            print("Hea poiss!")
+#        elif a==3:
+#            print("Pole hullu")
+#        elif a==2:
+#            print("sa pead proovima")
+#        else:
+#            print("no sa oled loll")
+#    except:
+#        print("Andmetüüp on vale või sa valesti vastas!")
